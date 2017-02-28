b80adc710c3495497b39266cb5ee30076f50fb97
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-28 14:20:07 -0500

    Updated from Brython Server: February 28, 2017 2:20:07 PM EST

--- distribution.py
+++ distribution.py
@@ -36,3 +36,35 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+
+text = input("Please enter a string of text (the bigger the better): ")
+s=list(text)
+ac=(s.count('a'))
+bc=(s.count('b'))
+cc=(s.count('c'))
+dc=(s.count('d'))
+ec=(s.count('e'))
+fc=(s.count('f'))
+gc=(s.count('g'))
+hc=(s.count('h'))
+ic=(s.count('i'))
+jc=(s.count('j'))
+kc=(s.count('k'))
+lc=(s.count('l'))
+mc=(s.count('m'))
+nc=(s.count('n'))
+oc=(s.count('o'))
+pc=(s.count('p'))
+qc=(s.count('q'))
+rc=(s.count('r'))
+sc=(s.count('s'))
+tc=(s.count('t'))
+uc=(s.count('u'))
+vc=(s.count('v'))
+wc=(s.count('w'))
+xc=(s.count('x'))
+yc=(s.count('y'))
+zc=(s.count('z'))
+
+print('The distribution of characters in "{0}" is: '.format())
+
