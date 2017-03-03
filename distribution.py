a4639b9179d7ac494ba18487d65ece41f17e654b
willl4 <williamlaycock@hanovernorwichschools.org>
2017-03-03 14:54:39 -0500

    Updated from Brython Server: March 3, 2017 2:54:39 PM EST

--- distribution.py
+++ distribution.py
@@ -94,7 +94,7 @@ def bsort(z, cmp):
     sorted = False  # assume the seq is not sorted to start with
     while not sorted:
         sorted = True   # assume it's already sorted correctly
-        for index, value in enumerate(seq): # for every element in seq
+        for index, value in enumerate(z): # for every element in seq
             if index > 0:                   # past the first..
                 if not cmp(z[index-1], value):  # if this element is out of order
                     sorted = False          # then the list is not sorted yet
